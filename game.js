3b6e3c52d56d2187dc7b84135fd15659941e03c1
Victoria Castro <92071913+vicchyx@users.noreply.github.com>
2022-03-23 02:37:34 +1100

    Js Script

--- game.js
+++ game.js
@@ -4,18 +4,133 @@ const progressText = document.querySelector('#progressText');
 const scoreText = document.querySelector('#score');
 const progressBarFull = document.querySelector('#progressBarFull');
 
-let currentQuestion = {}
-let acceptingAnswers = true
-let score = 0
-let questionCounter = 0
-let availableQuestions = []
+let timerCount = document.querySelector(".timer-count");
+let timerContainer = document.querySelector(".timer");
+let seconds = document.querySelector("#seconds");
+
+
+let currentQuestion = {};
+let acceptingAnswers = true;
+let score = 0;
+let index = 0;
+let timeLeft = 60;
+let deduction = 10;
+let questionCounter = 0;
+let availableQuestions = [];
 
 let questions = [
+    { 
+        // JavaScript - Logical Comparison
+        question: "What does the strict equality comparison operator (===) compare?",
+        choice1: "Value Only",
+        choice2: "Value and Type",
+        choice3: "Type Only",
+        choice4: "Numbers Only",
+        answer: "Value and Type",
+    },
     {
-        question: 'What is...',
-        choice1: '2',
-        choice2: '2',
-        choice3: '2',
-        choice4: '2',
+        // JavaScript - Logical Operator
+        question: "Which of the following is NOT a logical operator?",
+        choice1: "&&",
+        choice2: "!!",
+        choice3: "&|",
+        choice4: "||",
+        answer: "&|",
+    },
+
+    {
+        // JavaScript - HTML
+        question: "What do we use to dynamically manipulate the HTML elements on a page?",
+        choice1: "The DIM",
+        choice2: "The DEM",
+        choice3: "The DOM",
+        choice4: "The DMM",
+        answer: "The DOM",
+    }, 
+
+    {
+        //JavaScript - Primitive Types
+        question: "Which of the following are primitive data types?",
+        choice1: "Strings",
+        choice2: "Number",
+        choice3: "Undefined",
+        choice4: "All of the above",
+        answer: "All of the above",
+    },
+
+    {
+        //JavaScript - NaN
+        question: "What primitive type is NaN?",
+        choice1: "String",
+        choice2: "Number",
+        choice3: "Object",
+        choice4: "Undefined",
+        answer: "Number",
     }
+
 ]
+
+const MAX_QUESTIONS = 4
+
+startGame = () => {
+    questionCounter = 0
+    score = 60
+    availableQuestions = [...questions]
+    getNewQuestion ()
+}
+
+getNewQuestion = () => {
+    if(availableQuestions.length === 0 assign(url: String): void QUESTIONS) {
+        localStorage.setItem('mostRecentScore', score)
+
+        return window.location.assign ('/end.html')
+    } 
+
+    questionCounter ++ 
+    progressText.innerText = `Question ${questionCounter} of ${MAX_QUESTIONS}`
+    progressBarFull.style.width = `${(questionCounter/MAX_QUESTIONS) * 100}%`
+
+    const questionsIndex = Math.floor(Math.random() * availableQuestions.length)
+    currentQuestion = availableQuestions [questionsIndex]
+    question.innerText = currentQuestion.question
+
+    choices.forEach(choice => {
+        const Number = choice.dataset ['number']
+        choice.innerText = currentQuestion ['choice' + number]
+    })
+
+availableQuestions.splice(questionsIndex, 1)
+
+acceptingAnswers = true
+
+}
+
+choices.forEach(choice => {
+    choice.addEventListener ('click', e => {
+        if(!acceptingAnswers) return
+
+        acceptingAnswers = false
+        const selectedChoice = e.target
+        const selectedAnswer = selectedChoice.dataset ['number']
+
+        let classToApply = selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect'
+
+        if(classToApply === 'correct') {
+            incrementScore(SCORE_POINTS)
+        }
+
+        selectedChoice.parentElement.classList.add(classToApply)
+
+        setTimeout (() => {
+selectedChoice.parentElement.classList.remove(classToApply)
+getNewQuestion ()
+        }, 1000)
+    })
+})
+
+incrementScore = num => {
+    score +=num
+    scoreText.innerText = score
+}
+
+startGame ()
